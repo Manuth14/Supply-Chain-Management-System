aaf0f126441ca08803d3738bd6a3316a6bed020d
Manuth Thejaka <manuththejaka811@gmail.com>
2026-08-17 00:05:03 +0530

    Add filters

---
+++ scm-web/src/main/java/lk/jiat/scm/web/filter/AuthFilter.java
@@ -0,0 +1,30 @@
+package lk.jiat.scm.web.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/index.jsp", "/index", "/admin/*"})
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpResponse.setHeader("Pragma", "no-cache");
+        httpResponse.setDateHeader("Expires", 0);
+
+        HttpSession session = httpRequest.getSession(false);
+
+        if (session != null && session.getAttribute("user") != null) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+        }
+    }
+}
