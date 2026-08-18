8b12b4d0bd5fa157711c8bae1ad3fed6f2133ed9
Manuth Thejaka <manuththejaka811@gmail.com>
2026-08-18 18:10:08 +0530

    Add Product Entity

---
+++ scm-entities/src/main/java/lk/jiat/scm/entities/entity/Product.java
@@ -0,0 +1,47 @@
+package lk.jiat.scm.entities.entity;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
