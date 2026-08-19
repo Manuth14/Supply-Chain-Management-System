1621ce46f52b52836efd145fd13363e3ef717731
Manuth Thejaka <manuththejaka811@gmail.com>
2026-08-19 23:35:01 +0530

    Create TradeComplianceException

---
+++ scm-core/src/main/java/lk/jiat/scm/core/exceptions/TradeComplianceException.java
@@ -0,0 +1,7 @@
+package lk.jiat.scm.core.exceptions;
+
+public class TradeComplianceException extends Exception {
+    public TradeComplianceException(String message) {
+        super(message);
+    }
+}
