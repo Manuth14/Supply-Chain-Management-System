d90d4b92940ff00c09196441e6e0ece0a951204c
Manuth Thejaka <manuththejaka811@gmail.com>
2026-08-19 12:05:58 +0530

    Create custom order exception

---
+++ scm-core/src/main/java/lk/jiat/scm/core/exceptions/OrderProcessingException.java
@@ -0,0 +1,7 @@
+package lk.jiat.scm.core.exceptions;
+
+public class OrderProcessingException extends Exception {
+    public OrderProcessingException(String message) {
+        super(message);
+    }
+}
