a603b74cdb9e8741f0744119d6f3cec5ce4eeb8c
Manuth Thejaka <manuththejaka811@gmail.com>
2026-08-20 23:14:41 +0530

    Create DashboardServiceException & ResourceNotFoundException files

---
+++ scm-core/src/main/java/lk/jiat/scm/core/exceptions/DashboardServiceException.java
@@ -0,0 +1,7 @@
+package lk.jiat.scm.core.exceptions;
+
+public class DashboardServiceException extends Exception {
+    public DashboardServiceException(String message) {
+        super(message);
+    }
+}
---
+++ scm-core/src/main/java/lk/jiat/scm/core/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.jiat.scm.core.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
